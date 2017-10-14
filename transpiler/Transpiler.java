a4d90ab46447ef4a6a1c169fbdb729c7a5064e68
marsalad <marsalad@sas.upenn.edu>
2017-10-14 13:39:02 -0400

    added french and polish

--- transpiler/Transpiler.java
+++ transpiler/Transpiler.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.nio.file.Files;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -16,6 +15,8 @@ import com.google.googlejavaformat.java.Formatter;
 
 @SuppressWarnings("deprecation")
 public class Transpiler {
+    
+    // english keyword translations
     private static final String[] ENGLISH = new String[] {
             null, "abstract", "assert", "boolean", "break", "byte", "case", 
             "catch", "char", "class", "const", "continue", "default", 
@@ -27,21 +28,23 @@ public class Transpiler {
             "transient", "try", "void", "volatile", "while", null, null, 
             null, null, null, "null"
         };
-
-    private static final String[] ENGLISH_METHODS = {
-	        null, "clone", "equals", "finalize", "getClass", "hashCode", "notify",
-	        "notifyAll", "toString", "wait", "new", "arraycopy", "exit", 
-	        "setErr", "setIn", "setOut", "append", "checkError", "clearError",
-	        "close", "flush", "format", "print", "println", "write", "add", "remove",
-	        "next", "push", "pop", "copyOfRange", "addAll", "contains", "get", "indexOf",
-	        "isEmpty", "size", "length", "set", "peek", "search"
-	    };
     
+    /**
+     * Construct a tree of translatable method calls, then transpile the given files
+     * under the specified language.
+     * 
+     * @param args first element in language choice, remaining arguments are file
+     *             names without .cafe extensions
+     * @throws Exception if there is an error reading the translation txt
+     */
     public static void main(String[] args) throws Exception {
     	Map<String, String> methodMap = new TreeMap<String, String>();
-    	BufferedReader r = new BufferedReader(new FileReader(args[1] + ".txt"));
-
-        for (int i = 1; i < 107; i++) {
+    	BufferedReader r = new BufferedReader(new FileReader(args[0] + ".txt"));
+        @SuppressWarnings("unchecked")
+        Class<? extends Lexer> c = (Class<? extends Lexer>) Class.forName(args[0] + "Lexer");
+        
+    	// get method translations
+    	for (int i = 1; i < 107; i++) {
         	String line = r.readLine();
         	if (52 < i) {
         		int divInd = line.indexOf('=');
@@ -49,45 +52,51 @@ public class Transpiler {
                               line.substring(0, divInd));
         	}
         }
+        r.close();
 	
-        CharStream s = new ANTLRFileStream(args[0] + ".cafe");
-        TokenStream t;
-        try {
-            Class<? extends Lexer> c = Language.languageOfString(args[1]).getLexer();
-            t = new CommonTokenStream(c.getConstructor(CharStream.class).newInstance(s));
-        } catch (Exception e) {
-            throw e;
-        }
-        StringBuilder output = new StringBuilder();
-        
-        int i = 1;
-        boolean dotSeen = false;
-        while (true) {
-            int id = t.LA(i);
-            if (id < 1) {
-                break;
-            } else if (dotSeen) {
-            	String token = methodMap.get(t.LT(i).getText());
-            	if (token != null) {
-            		output.append(token);	
-            	} else {
-            		output.append(t.LT(i).getText());	
-            	}
-            	dotSeen = false;
-            } else if (id < ENGLISH.length - 6 || id == ENGLISH.length - 1) {
-                output.append(ENGLISH[id] + " ");
-            } else if (id == 65) {
-            	dotSeen = true;
-            	output.append(t.LT(i).getText() + " ");
-            } else {
-                output.append(t.LT(i).getText() + " ");
+        // transpile every token in the file
+        for (int fnum = 1; fnum < args.length; fnum++) {
+            CharStream s = new ANTLRFileStream(args[fnum] + ".cafe");
+            TokenStream t = new CommonTokenStream(c.getConstructor(CharStream.class).newInstance(s));
+            StringBuilder output = new StringBuilder();
+            
+            int i = 1;
+            boolean dotSeen = false;
+            while (true) {
+                int id = t.LA(i); // get token id number
+                
+                // end of stream
+                if (id < 1) {
+                    break;
+                    
+                // transpile method
+                } else if (dotSeen) {
+                    String token = methodMap.get(t.LT(i).getText());
+                    if (token != null) {
+                        output.append(token);   
+                    } else {
+                        output.append(t.LT(i).getText());   
+                    }
+                    dotSeen = false;
+                    
+                // transpile keyword
+                } else if (id < ENGLISH.length - 6 || id == ENGLISH.length - 1) {
+                    output.append(ENGLISH[id] + " ");
+                
+                // not a keyword or method token, use original
+                } else {
+                    if (id == 65) {
+                        dotSeen = true;
+                    }
+                    output.append(t.LT(i).getText() + " ");
+                }
+                i++;
             }
-            i++;
+            
+            // format and write transpiled file as java
+            BufferedWriter writer = new BufferedWriter(new FileWriter(args[fnum] + ".java"));
+            writer.write(new Formatter().formatSource(output.toString()));
+            writer.close();
         }
-        
-        BufferedWriter writer = new BufferedWriter(new FileWriter(args[0] + ".java"));
-        writer.write(new Formatter().formatSource(output.toString()));
-        writer.close();
-
     }
 }
