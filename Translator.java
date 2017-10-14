973e8c0f3c03da8a65971070f5abbae810d5f54c
Angelo Saraceno <asara019@fiu.edu>
2017-10-14 02:30:29 -0400

    weak ass changes

--- Translator.java
+++ Translator.java
@@ -23,6 +23,45 @@ public class Translator {
             "transient", "try", "void", "volatile", "while", null, null, 
             null, null, null, "null"
         };
+
+    private static final String[] ENGLISH_METHODS = {
+        null, "clone", "equals", "finalize", "getClass", "hashCode", "notify",
+        "notifyAll", "toString", "wait", "new", "arraycopy", "exit", 
+        "setErr", "setIn", "setOut", "append", "checkError", "clearError",
+        "close", "flush", "format", "print", "println", "write", "add", "remove",
+        "next", "push", "pop", "copyOfrange", "addAll", "contains", "get", "indexOf",
+        "isEmpty", "size", "length", "set", "peek", "search"
+    };
+
+@Override public void enterMethodDeclaration(Java7Parser.MethodDeclarationContext ctx) {
+    TerminalNode identifier = ctx.Identifier();
+    if (identifier!=null) {
+        for (String name:methodNamesToMatch) {
+        if (identifier.getText().equals(name)) {
+            int a = ctx.start.getStartIndex();
+            int b = ctx.stop.getStopIndex();
+            Interval interval = new Interval(a,b);
+            intervals.add(interval);
+        }
+        }
+    }
+    }
+
+    private void writeOutputFile() {
+    File outFile = new File(this.outputDir, this.currentJavaFile.getName());
+    try {
+        FileWriter fw = new FileWriter(outFile);
+        for (Interval interval:this.intervals) {
+        fw.write(input.getText(interval));
+        fw.write("\n\n");
+        }
+        fw.close();
+    } catch (Exception e) {
+        System.err.println("Could not write output file " + outFile);
+        e.printStackTrace();
+        }
+    }
+  
     
     public static void main(String[] args) throws IOException, FormatterException {
         CharStream s = new ANTLRFileStream(args[0] + ".cafe");
@@ -43,5 +82,7 @@ public class Translator {
         BufferedWriter writer = new BufferedWriter(new FileWriter(args[0] + ".java"));
         writer.write(new Formatter().formatSource(output.toString()));
         writer.close();
+
+        
     }
 }
